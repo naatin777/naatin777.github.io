c7a753322a7c6ec3a6c3b20c7bcc8ea6520218e5
tknk0369 <76672482+tknk0369@users.noreply.github.com>
2022-11-03 13:20:17 +0900

    Setup Chakra UI

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,17 @@
+import { Heading, Stack } from '@chakra-ui/react'
 import Head from 'next/head'
 
 export default function Index() {
   return (
-    <div>
-      <h1>Test</h1>
-    </div>
+    <>
+      <Head>
+        <title>tknk0369</title>
+      </Head>
+      <Stack align='center' m={4}>
+        <Heading as='h1' size='xl'>
+          Test
+        </Heading>
+      </Stack>
+    </>
   )
 }
