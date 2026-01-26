47e58c5d87535b0938fa7d526154e5f6c0dc9223
naatin777 <76672482+naatin777@users.noreply.github.com>
2026-01-26 13:20:41 +0900

    Add global styles and initial CSS reset

---
+++ src/styles/global.css.ts
@@ -0,0 +1,20 @@
+import { globalStyle } from "@vanilla-extract/css";
+import { themeVars } from "./theme.css";
+
+globalStyle("*, *::before, *::after", {
+  boxSizing: "border-box",
+});
+
+globalStyle("body", {
+  backgroundColor: themeVars.color.background,
+  color: themeVars.color.text,
+  fontFamily: "'Inter', 'Noto Sans JP', sans-serif",
+  minHeight: "100vh",
+  margin: 0,
+  padding: 0,
+});
+
+globalStyle("img", {
+  maxWidth: "100%",
+  height: "auto",
+});
