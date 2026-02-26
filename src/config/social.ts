e8c7afb88f27b4df48d99679bdb024bc64606aa3
naatin777 <76672482+naatin777@users.noreply.github.com>
2026-02-26 11:15:25 +0900

    fix(assets): update sns icon import paths
    Change social icon imports to reference the assets directory
    (@assets/sns) instead of the old @sns root. This consolidates
    static social media icon paths, fixes broken imports, and ensures
    icons are resolved correctly by the bundler and asset pipeline.

--- src/config/social.ts
+++ src/config/social.ts
@@ -1,14 +1,14 @@
 import type { ImageMetadata } from "astro";
-import BlueskyDarkIcon from "@sns/bluesky_dark.svg";
-import BlueskyLightIcon from "@sns/bluesky_light.svg";
-import GitHubDarkIcon from "@sns/github_dark.svg";
-import GitHubLightIcon from "@sns/github_light.svg";
-import QiitaDarkIcon from "@sns/qiita_dark.png";
-import QiitaLightIcon from "@sns/qiita_light.png";
-import XDarkIcon from "@sns/x_dark.png";
-import XLightIcon from "@sns/x_light.png";
-import ZennDarkIcon from "@sns/zenn_dark.svg";
-import ZennLightIcon from "@sns/zenn_light.svg";
+import BlueskyDarkIcon from "@assets/sns/bluesky_dark.svg";
+import BlueskyLightIcon from "@assets/sns/bluesky_light.svg";
+import GitHubDarkIcon from "@assets/sns/github_dark.svg";
+import GitHubLightIcon from "@assets/sns/github_light.svg";
+import QiitaDarkIcon from "@assets/sns/qiita_dark.png";
+import QiitaLightIcon from "@assets/sns/qiita_light.png";
+import XDarkIcon from "@assets/sns/x_dark.png";
+import XLightIcon from "@assets/sns/x_light.png";
+import ZennDarkIcon from "@assets/sns/zenn_dark.svg";
+import ZennLightIcon from "@assets/sns/zenn_light.svg";
 import { author } from "./author";
 
 export interface SocialLink {
