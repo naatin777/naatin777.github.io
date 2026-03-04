e4822724270e60493c87870d2f2b29beb3d43a2c
naatin777 <76672482+naatin777@users.noreply.github.com>
2026-03-04 21:58:32 +0900

    refactor(assets): move social icons into social directoryUpdate imports in src/config/social.ts to point to renamed assetfiles under the social directory. This consolidates social networkicons into a single folder (social) and updates paths from theprevious sns/ naming to the new dash-separated filenames.
    This change clarifies asset organization and makes icon pathsconsistent across light/dark variants, reducing ambiguity whenadding or locating social icons.

--- src/config/social.ts
+++ src/config/social.ts
@@ -1,14 +1,14 @@
 import type { ImageMetadata } from "astro";
-import BlueskyDarkIcon from "@assets/sns/bluesky_dark.svg";
-import BlueskyLightIcon from "@assets/sns/bluesky_light.svg";
-import GitHubDarkIcon from "@assets/sns/github_dark.svg";
-import GitHubLightIcon from "@assets/sns/github_light.svg";
-import QiitaDarkIcon from "@assets/sns/qiita_dark.png";
-import QiitaLightIcon from "@assets/sns/qiita_light.png";
-import XDarkIcon from "@assets/sns/x_dark.png";
-import XLightIcon from "@assets/sns/x_light.png";
-import ZennDarkIcon from "@assets/sns/zenn_dark.svg";
-import ZennLightIcon from "@assets/sns/zenn_light.svg";
+import BlueskyDarkIcon from "@assets/social/bluesky-dark.svg";
+import BlueskyLightIcon from "@assets/social/bluesky-light.svg";
+import GitHubDarkIcon from "@assets/social/github-dark.svg";
+import GitHubLightIcon from "@assets/social/github-light.svg";
+import QiitaDarkIcon from "@assets/social/qiita-dark.png";
+import QiitaLightIcon from "@assets/social/qiita-light.png";
+import XDarkIcon from "@assets/social/x-dark.png";
+import XLightIcon from "@assets/social/x-light.png";
+import ZennDarkIcon from "@assets/social/zenn-dark.svg";
+import ZennLightIcon from "@assets/social/zenn-light.svg";
 import { author } from "./author";
 
 export interface SocialLink {
